be1d8ae61b2bacb5941e6125758ab7b523a9b723
Dmdv <dimos-d@yandex.ru>
2015-01-27 19:47:19 +0300

    Renamed to NetCommon
    Renamed to NetCommon, added nuget creation tools

--- Common/Properties/AssemblyInfo.cs
+++ Common/Properties/AssemblyInfo.cs
@@ -1,25 +1,27 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Common")]
+
+[assembly: AssemblyTitle("NetCommon")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ZAO UniCredit Bank")]
-[assembly: AssemblyProduct("Common")]
-[assembly: AssemblyCopyright("Copyright © ZAO UniCredit Bank 2013")]
+[assembly: AssemblyCompany("Dyachkov Dmitry")]
+[assembly: AssemblyProduct("NetCommon")]
+[assembly: AssemblyCopyright("Copyright © Dyachkov Dmitry 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
 // для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
 // COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+
 [assembly: Guid("51a80712-2637-44e4-8b97-aa5b5bd3c3f3")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
@@ -32,5 +34,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.4.5")]
-[assembly: AssemblyFileVersion("1.1.4.5")]
+
+[assembly: AssemblyVersion("1.1.4.6")]
+[assembly: AssemblyFileVersion("1.1.4.6")]
